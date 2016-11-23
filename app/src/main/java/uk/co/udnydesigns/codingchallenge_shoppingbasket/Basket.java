4c24596d505736d6d27ce103a79ae28b62543cc3
Adrian Tuckwell <adrian.tuckwell@gmail.com>
2016-11-23 15:10:29 +0000

    refoactored shopping basket

--- app/src/main/java/uk/co/udnydesigns/codingchallenge_shoppingbasket/Basket.java
+++ app/src/main/java/uk/co/udnydesigns/codingchallenge_shoppingbasket/Basket.java
@@ -38,56 +38,71 @@ public class Basket
         return basket.size();
     }
 
-    public Double value()
+    // remove 20% discount if > £20
+    public  Double discount(Double subtotal)
     {
+        Double result = 0.0;
+        if (subtotal >= 20.00){
 
-        Double subtotal = 0.0;
-        Double total1 = 0.0;
-        Double total2 = 0.0;
+            result = (subtotal/100.0)*90.0;
+        }
+        else
+        {
+            result = subtotal;
+        }
+        return result;
+    }
 
-        String bogoff= "";
+
+    public Double loyaltyCard (Double subtotal)
+    {
+        Double result = 0.0;
+        if (customer.getCard()){
+
+            result = (subtotal/100.0)*98.0;
+        }
+        else
+        {
+            result = subtotal;
+        }
+        return result;
+    }
+
+
+    public Double bogof (Double subtotal)
+    {
+        Double result = 0.0;
+        String bogof= "";
 
         for (Item item: this.basket)
         {
-           if (bogoff == item.getName())
+            if (bogof == item.getName())
             {
-                //if bogoff is the same as last item reset
-                bogoff = "";
+                //if bogof is the same as last item reset
+                bogof = "";
 
             } else
             {
                 //Store the name of the item
-                bogoff = item.getName();
-                subtotal += item.getPrice();
+                bogof = item.getName();
+                result += item.getPrice();
             }
         }
-
-        // if total greater than 20 give 10%
-        if (subtotal >= 20.00){
-
-            total1 = (subtotal/100.0)*90.0;
-        }
-        else
-        {
-            total1 = subtotal;
-        }
-
-        // customer has discount card 2%
-        if (customer.getCard()){
-
-            total2 = (total1/100.0)*98.0;
-        }
-        else
-        {
-            total2 = total1;
-        }
-
-        return total2;
+        return result;
     }
 
+    // sum basket and apply bogof and 20% discount and loyalty card
+    public Double value()
+    {
+        Double subtotal = 0.0;
+        // sum basket and check for bogof
+        subtotal= bogof(subtotal);
+        // remove 20% discount if > £20
+        subtotal = discount(subtotal);
+        // remove loyalty discount if customer has loyalty card
+        subtotal = loyaltyCard(subtotal);
 
-
-
-
+        return subtotal;
+    }
 }
 
